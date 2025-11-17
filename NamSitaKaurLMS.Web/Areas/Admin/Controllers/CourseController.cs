ec75ce6760d1e98a8d01ddf51df2a172909935e7
Oğuz Alp ARSLAN <oguzalparsln@gmail.com>
2025-11-17 22:05:45 +0300

    Add project files.

---
+++ NamSitaKaurLMS.Web/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NamSitaKaurLMS.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DeactiveCourse()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCourse()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCourseContent()
+        {
+            return View();
+        }
+
+        public IActionResult CourseCalendar()
+        {
+            return View();
+        }
+    }
+}
---
+++ NamSitaKaurLMS.Web/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Areas", "Admin", "Static")),
+    RequestPath = "/admin-static"
+});
+
+app.MapStaticAssets();
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
