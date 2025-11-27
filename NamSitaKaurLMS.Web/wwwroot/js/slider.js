cb42dc45bc2a9e717de0834da63282698cfd1c03
Oğuz Alp ARSLAN <oguzalparsln@gmail.com>
2025-11-27 23:57:38 +0300

    UI Entegrasyonu

---
+++ NamSitaKaurLMS.Web/wwwroot/js/slider.js
@@ -0,0 +1,2 @@
+// Placeholder for a more advanced testimonial slider if you want to extend later.
+// Currently testimonials are static Bootstrap cards.
---
+++ NamSitaKaurLMS.Web/wwwroot/js/ui-interactions.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simple fade-in effect already handled by CSS animation classes
+    // Additional interactive behaviors can be added here.
+});
